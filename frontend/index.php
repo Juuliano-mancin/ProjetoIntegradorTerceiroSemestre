76f399c66d8f338e0e7414fd33e356a3c552db1b
Juliano Mancini <juuliano.mancin@gmail.com>
2025-06-06 15:32:02 -0300

    page: index.php
    Página de boas vindas para o sistema.

---
+++ frontend/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Bem-vindos</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="estilo.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" />
+</head>
+
+<body>
+  <header class="p-3">
+    <h1 class="fw-bold ms-3">Sistema de Gestão Escolar</h1>
+  </header>
+
+  <main class="container text-center my-5">
+    <h1 class="fw-bold">BEM-VINDOS</h1>
+    <p class="fs-4">Colégio Sanquim</p>
+
+    <div class="mt-5">
+      <a href="page_admin.html" class="btn btn-primary btn-lg">Entrar no Sistema</a>
+    </div>
+  </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
